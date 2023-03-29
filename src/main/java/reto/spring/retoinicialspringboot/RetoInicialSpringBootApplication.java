5ac04950edc93220820d37d6149f8092b8c19cf1
Adrian de la Fuente Garceso <adelafug@emeal.nttdata.com>
2023-03-29 16:46:20 +0200

    RetoInicial Spring Boot FCT NNT Data

---
+++ src/main/java/reto/spring/retoinicialspringboot/RetoInicialSpringBootApplication.java
@@ -0,0 +1,13 @@
+package reto.spring.retoinicialspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoInicialSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoInicialSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reto/spring/retoinicialspringboot/RetoInicialSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package reto.spring.retoinicialspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoInicialSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
